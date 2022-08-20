17ec8a13a3586978215d550430d05916ad8db9e0
Eugene Rudakov <voragomod@icloud.com>
2022-08-20 18:08:47 +0300

    add Sourcery support

---
+++ MVVM_Combine/Autogenerated/AutoInjection.generated.swift
@@ -0,0 +1,7 @@
+// Generated using Sourcery 1.8.1 — https://github.com/krzysztofzablocki/Sourcery
+// DO NOT EDIT
+// swiftlint:disable all
+
+// MARK: - AutoInjection for structs
+
+// swiftlint:enable all
